77d80c372926d3ae99d27bf60f71c2480c45da59
Geonovum <>
2023-01-15 09:58:31 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/ff33b83e483280947cbbddcacb5ac3b7afbf7f34

--- js/config.js
+++ js/config.js
@@ -1,12 +1,19 @@
 var respecConfig =
 {
+  subtitle: "Gebruikershandleiding",
   pubDomain: "geen",
   specStatus: "GN-BASIS",
   specType: "AL",
   license: "cc-by-nd",
   publishDate: "2023-01-14",
+  previousPublishDate: "2023-01-10",
+  previousMaturity: "geen",
   content: {
-    "section_000": ""
+    "section_000": "",
+    "section_001": "",
+    "section_002": "",
+    "section_003": "",
+    "section_004": ""
   },
   authors: [
     {
@@ -24,6 +31,8 @@ var respecConfig =
   ],
   shortName: "respec",
   format: "markdown",
+  github: "https://github.com/Geonovum/geen-respec/",
+  issueBase: "https://github.com/Geonovum/geen-respec/issues/",
   edDraftURI: "https://Geonovum.github.io/geen-respec/",
   publishISODate: "2023-01-14T00:00:00.000Z",
   generatedSubtitle: "Document 14 januari 2023"
