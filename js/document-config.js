49bbd8ddd137ec5c93a8c31dc1df4f4b51df25b0
Gerard Wolbers <g.wolbers@geonovum.nl>
2023-08-02 13:28:49 +0200

    Gebruikershandleiding word2respec 1.1.0

---
+++ js/document-config.js
@@ -0,0 +1,43 @@
+var documentConfig =
+{
+  title: "Gebruikershandleiding word2respec",
+  shortName: "respec",
+  pubDomain: "wp",
+  specStatus: "basis",
+  specType: "bd",
+  license: "cc-by-nd",
+  latestVersion: [
+    "docs.geostandaarden.nl/wp/respec/"
+  ],
+  publishDate: "2023-08-02",
+  publishVersion: "1.1.0",
+  thisVersion: [
+    "docs.geostandaarden.nl/wp/basis-bd-respec-20230802/"
+  ],
+  previousPublishDate: "2023-01-14",
+  previousPublishVersion: "1.0.0",
+  prevVersion: [
+    "docs.geostandaarden.nl/wp/geen-bd-respec-20230114/"
+  ],
+  previousMaturity: "geen",
+  github: "https://github.com/geen/wp-respec/",
+  issueBase: "https://github.com/geen/wp-respec/issues/",
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  localBiblio: {
+    "DONL": {
+      title: "data.overheid.nl",
+      href: "https://data.overheid.nl",
+      authors: "",
+      publisher: "Kennis- en exploitatiecentrum voor Officiële Overheidspublicaties",
+      editors: "",
+      date: "",
+      status: ""
+    }
+  }
+}
